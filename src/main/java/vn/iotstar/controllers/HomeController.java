26fa93eabc84179650a12edf193e07b3d9293f93
Vietfinn <vietfinn9@gmail.com>
2025-09-10 15:58:14 +0700

    Update Sitemesh Decorator 3 and Template Bootstrap

--- src/main/java/vn/iotstar/controllers/HomeController.java
+++ src/main/java/vn/iotstar/controllers/HomeController.java
@@ -17,6 +17,6 @@ public class HomeController extends HttpServlet{
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.getRequestDispatcher("/views/home.jsp").forward(req, resp);
 	}
-	
+	//helooo
 
 }
